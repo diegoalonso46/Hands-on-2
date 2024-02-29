190c172e8b3bf76ffb4ce05885650ce69881a2dc
diegoalonso46 <124904176+diegoalonso46@users.noreply.github.com>
2024-02-28 23:52:35 -0600

    Create helloworld.sh

---
+++ Hands-on 3/helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Imprimir "Hello World" en pantalla
+echo "Hello World"
+
+# Listar el contenido del directorio actual
+ls -1
+
+# Crear un directorio llamado "Test"
+mkdir Test
+
+# Cambiarse al directorio "Test"
+cd Test
+
+# Listar el contenido del directorio "Test"
+ls -1
